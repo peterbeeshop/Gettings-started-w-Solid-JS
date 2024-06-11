b3d83ea16ab8813e117c19de3493be3f02185fe5
peter muyambango <muyambangopeter@gmail.com>
2024-06-11 14:53:19 +0200

    initial push

---
+++ src/components/AlertDialog/index.tsx
@@ -0,0 +1,63 @@
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogContentText,
+    DialogTitle,
+    Slide,
+} from "@suid/material";
+import { TransitionProps } from "@suid/material/transitions";
+import { Accessor, createSignal, JSXElement} from "solid-js";
+import {ITodo} from "../Todo";
+
+interface IDialog {
+  todo: ITodo;
+  todos: Accessor<ITodo[]>;
+  setTodos: (value: ITodo[] | ((prev: ITodo[]) => ITodo[])) => void;
+  isDialogOpen: Accessor<boolean>;
+  setIsDailogOpen: (isDialogOpen: boolean) => void;
+}
+  
+const Transition = function Transition(
+  props: TransitionProps & {
+    children: JSXElement;
+  }
+) {
+  return <Slide direction="up" {...props} />;
+};
+  
+  export default function AlertDialog({todo, todos, setTodos, isDialogOpen, setIsDailogOpen}: IDialog) {
+    const [updatedTitle, setUpdatedTitle] = createSignal('');
+    const handleClose = () => {
+      setIsDailogOpen(!isDialogOpen);
+    };
+
+    const updateTodoTitle = (id: number) => {
+      const updatedTodos = todos().map(todo => todo.id === id ? {...todo, title: updatedTitle()}: todo);
+      setTodos(updatedTodos)
+      setIsDailogOpen(!isDialogOpen);
+    }
+  
+    return (
+      <div>
+        <Dialog
+          open={isDialogOpen()}
+          TransitionComponent={Transition}
+          onClose={handleClose}
+          aria-describedby="alert-dialog-slide-description"
+        >
+          <DialogTitle>{"Use Google's location service?"}</DialogTitle>
+          <DialogContentText id="alert-dialog-slide-description">{todo.title}</DialogContentText>
+          <DialogContent>
+            <input type="text" value={updatedTitle()} onInput={(e) => setUpdatedTitle(e.currentTarget.value)} />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={() => updateTodoTitle(todo.id)}>Update</Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+    );
+  }
+  
---
+++ src/components/Todo/index.tsx
@@ -0,0 +1,62 @@
+import { Component, createSignal } from 'solid-js'
+import './index.css'
+import DeleteIcon from "@suid/icons-material/Delete";
+import PencilIcon from "@suid/icons-material/Create";
+import AlertDialog from '../AlertDialog';
+
+export interface ITodo {
+    id: number;
+    title: string;
+    isCompleted: boolean;
+}
+
+const Todo: Component = () => {
+    const [todos, setTodos] = createSignal<ITodo[]>([]);
+    const [text, setText] = createSignal<string>('')
+    const [isDialogOpen, setIsDialogOpen] = createSignal(false);
+
+    const addTodo = () => {
+        setTodos([...todos(), {id: Math.floor(Math.random() * 100), title: text(), isCompleted: false}])
+        setText('');
+    }
+
+    const toggleIsTodoComplete = (id: number) => {
+        const updatedTodos = todos().map(todo => todo.id === id ? {...todo, isCompleted: !todo.isCompleted}: todo);
+        setTodos(updatedTodos);
+    }
+
+    const deleteTodo = (id: number) => {
+        const updatedTodo = todos().filter(todo => todo.id !== id);
+        setTodos(updatedTodo);
+    }
+
+    return (
+      <div class='container'>
+        <h2>Todos</h2>
+        <input type='text' value={text()} onInput={(e) => setText(e.currentTarget.value)} />
+        <button onClick={addTodo}>Create Todo</button>
+        <h3>My Todos</h3>
+        {
+            todos().map(todo => {
+                const {id, title, isCompleted} = todo;
+                return (
+                    <>
+                        <AlertDialog todo={todo} todos={todos} setTodos={setTodos} isDialogOpen={isDialogOpen} setIsDailogOpen={setIsDialogOpen} />
+                        <div class='todo-container'>
+                            <input onClick={() => toggleIsTodoComplete(id)} type='checkbox' id={title} checked={isCompleted} />
+                            {isCompleted? <h3><s>{title}</s></h3> : <h3>{title}</h3>}
+                            <PencilIcon onclick={() => {
+                                setIsDialogOpen(!isDialogOpen());
+                                console.log('isDialogOpen', isDialogOpen());
+                            }} color='info' />
+                            <DeleteIcon onclick={() => deleteTodo(id)} color='error' />
+                        </div>
+                    </>
+                )
+            })
+        }
+      </div>
+    )
+  }
+  
+  export default Todo
